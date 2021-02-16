2de96296b68f5ea23d2bad5e8b9d544de5cc3e02
Andre Susantin <andre.susantin@google.com>
2021-02-15 18:28:05 -0800

    remove duplicate

--- bank-account/pangram/pangram.js
+++
@@ -1,6 +0,0 @@
-
-const atoz = "abcdefghijklmnopqrstuvwxyz"
-export const isPangram = (input) => {
-  let inputLowcase = input.toLowerCase()
-  return [...atoz].every( chr => inputLowcase.includes(chr))
-};
--- bank-account/run-length-encoding/run-length-encoding.js
+++
@@ -1,7 +0,0 @@
-
-export const encode = (input) => 
-  // use replace regex with group binding
-  input.replace(/(.)\1+/g, (charGroup,char) => charGroup.length + char )
-  
-
-export const decode = (input) => input.replace(/(\d+)(.)/g, ( _, count, char) => char.repeat(count))
